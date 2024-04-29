06543169df96317fcf82243da3e410dfaa4c5398
Lukáš Kudělka <kudelkal@office365.zcu.cz>
2024-04-29 14:33:56 +0200

    Přidejte soubory projektu.

---
+++ PGM2_SVG_Animace/Globals/Enums.cs
@@ -0,0 +1,10 @@
+﻿namespace PGM2_SVG_Animace.Globals
+{
+    public enum TypObrazce
+    {
+        ctverec = 0,
+        obdelnik = 1,
+        kruh = 2,
+
+    }
+}
---
+++ PGM2_SVG_Animace/Models/Kruh.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+
+namespace PGM2_SVG_Animace.Models
+{
+    public class Kruh : Obrazec
+    {
+        public Kruh(Globals.TypObrazce typObrazce,int pozX, int pozY, Color barva, int polomer) : base(typObrazce, pozX, pozY, barva)
+        {
+            Polomer = polomer;
+        }
+        public int Polomer {  get; set; }
+        
+    }
+}
